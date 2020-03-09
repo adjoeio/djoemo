6a68fa603790054105ec00c0f24bea9e5550763c
Abdelilah Benhima <abdelilah@adjoe.io>
2020-03-09 17:39:48 +0100

    refs #11: handle "add" update expression;

--- update_expression.go
+++ update_expression.go
@@ -13,3 +13,7 @@ const SetIfNotExists UpdateExpression = "SetIfNotExists"
 
 // SetExpr performs a custom set expression, substituting the args into expr as in filter expressions.
 const SetExpr UpdateExpression = "SetExpr"
+
+// Add increments the path value in case of a number, or in case of a set it appends to that set.
+// If a prior value doesn't exist it will set the path to that value.
+const Add UpdateExpression = "ADD"
