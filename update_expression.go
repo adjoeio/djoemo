d195aea38a6f342ded05b5ff0159bd41c480cd33
Dorian Offermann <dorian@adjoe.io>
2020-03-17 17:52:41 +0100

    refs #12: Added UpdateWithUpdateExpressions method;
        - added custom update expression type

--- update_expression.go
+++ update_expression.go
@@ -17,3 +17,7 @@ const SetExpr UpdateExpression = "SetExpr"
 // Add increments the path value in case of a number, or in case of a set it appends to that set.
 // If a prior value doesn't exist it will set the path to that value.
 const Add UpdateExpression = "ADD"
+
+// UpdateExpressions is a type alias used for specifiyng multiple
+// update expressions at once
+type UpdateExpressions map[UpdateExpression]map[string]interface{}
