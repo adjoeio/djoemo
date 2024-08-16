e10d522100f0286e546eb30df1a54d8d1d05ad7f
Sergei Sadov <32732298+SergeiSadov@users.noreply.github.com>
2024-08-16 15:12:10 +0200

    add next item with error

--- iterator.go
+++ iterator.go
@@ -2,6 +2,7 @@ package djoemo
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/guregu/dynamo"
@@ -10,6 +11,7 @@ import (
 // IteratorInterface ...
 type IteratorInterface interface {
 	NextItem(out interface{}) bool
+	NextItemWithError(out interface{}) (bool, error)
 }
 
 // Iterator ...
@@ -32,4 +34,23 @@ func (itr *Iterator) NextItem(out interface{}) bool {
 		return itr.iterator.NextWithContext(itr.ctx, out)
 	}
 	return more
+}
+
+func (itr *Iterator) NextItemWithError(out interface{}) (bool, error) {
+	more := itr.iterator.NextWithContext(itr.ctx, out)
+	if err := itr.iterator.Err(); err != nil {
+		return false, fmt.Errorf("initial iterator.NextWithContext: %w", err)
+	}
+
+	if !more && itr.iterator.LastEvaluatedKey() != nil {
+		itr.scan = itr.scan.StartFrom(itr.iterator.LastEvaluatedKey())
+		itr.iterator = itr.scan.Iter()
+
+		more = itr.iterator.NextWithContext(itr.ctx, out)
+		if err := itr.iterator.Err(); err != nil {
+			return false, fmt.Errorf("subsequent iterator.NextWithContext: %w", err)
+		}
+	}
+
+	return more, nil
 }
